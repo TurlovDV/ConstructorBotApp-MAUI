d35265156dd5e9f533ca71c239007e4d7e04bf50
TurlovDV <124359442+TurlovDV@users.noreply.github.com>
2023-07-10 00:28:13 +0300

    commit

--- ConstructorBot/ConstructorBot/obj/Debug/net7.0-android/android/src/com/microsoft/maui/R.java
+++ ConstructorBot/ConstructorBot/obj/Debug/net7.0-android/android/src/com/microsoft/maui/R.java
@@ -1716,8 +1716,8 @@ public final class R {
 		public static final int notification_template_icon_low_bg = 0x7f07010b;
 		public static final int notification_tile_bg = 0x7f07010c;
 		public static final int notify_panel_notification_icon_bg = 0x7f07010d;
-		public static final int tooltip_frame_dark = 0x7f07011e;
-		public static final int tooltip_frame_light = 0x7f07011f;
+		public static final int tooltip_frame_dark = 0x7f07011f;
+		public static final int tooltip_frame_light = 0x7f070120;
 	}
 	public static final class id {
 		public static final int BOTTOM_END = 0x7f080001;
